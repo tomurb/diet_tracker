994084d68ca657fba1881d580efc73d3e944e081
Tomek Urban <tomasz.urban@sparksolutions.co>
2019-05-05 20:02:13 +0200

    Use format in url_helper for choosing render method

--- app/controllers/weight_logs_controller.rb
+++ app/controllers/weight_logs_controller.rb
@@ -3,12 +3,13 @@ class WeightLogsController < ApplicationController
     @weight_logs = current_user.biometric.weight_logs.order(:date)
     @from = from
     @to = to
-    @charted_weight_logs = WeightLogsToChartData.new(@weight_logs, @from, @to).call
-    flash.now[:error] = 'From should not be later than to' if @from > @to
-    if params[:commit] == 'CSV'
-      csv_data = ::WeightLogsToCsv.new(current_user.biometric.weight_logs).call
-      csv_filename = "weight_logs_#{Date.today}.csv"
-      send_data csv_data, filename: csv_filename
+    if @from > @to
+      handle_wrong_dates
+    else
+      respond_to do |format|
+        format.html { handle_html_index }
+        format.csv { handle_csv_index }
+      end
     end
   end
 
@@ -28,6 +29,25 @@ class WeightLogsController < ApplicationController
   end
 
   private
+
+  def handle_html_index
+    @charted_weight_logs = WeightLogsToChartData.new(@weight_logs,
+                                                     @from, @to).call
+  end
+
+  def handle_csv_index
+    csv_filename = "weight_logs_#{Date.today}.csv"
+    send_data weight_logs_csv_data, filename: csv_filename
+  end
+
+  def handle_wrong_dates
+    flash.now[:error] = 'From should not be later than to'
+    render template: 'weight_logs/index.html'
+  end
+
+  def weight_logs_csv_data
+    ::WeightLogsToCsv.new(@weight_logs.where(date: @from..@to)).call
+  end
 
   def weight_log_params
     params.require(:weight_log).permit(:date, :weight)
